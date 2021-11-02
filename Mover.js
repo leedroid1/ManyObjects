c2d3a4867a22dcc9ed7575fa0639fa9159290277
leedroid1 <leedroid800@gmail.com>
2021-11-02 16:15:07 +0900

    add brush

--- Mover.js
+++ Mover.js
@@ -4,28 +4,53 @@ class Mover {
     this.vel = createVector();
     this.acc = createVector();
     this.m = m;
-    
   }
-  
+
   applyForce(aForce) {
     let f = p5.Vector.div(aForce, this.m);
     this.acc.add(f);
   }
-  
+
+  attractTo(aPos){
+    let force= p5.Vector.sub(aPos, this.pos);
+    force.limit(3);
+    this.applyForce(force);
+  }
+
   update() {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.set(0, 0);
   }
-  
- attractTo(aPos) {
-    let f = p5.Vector.sub(aPos, this.pos)
-    f.limit(2);
-    this.applyForce(f);
- }
-  
+
   show() {
-    fill(0);
+    noStroke();
+    fill(250);
     circle(this.pos.x, this.pos.y, this.m);
+  }
+
+  drawOn(canvas) {
+    canvas.fill(180, 0, 0, 20);
+    canvas.noStroke();
+    canvas.circle(this.pos.x, this.pos.y, this.vel.mag()*this.m/4);
+  }
+
+  edges(){
+    if (this.pos.y > height) {
+      this.vel.y *= -1;
+      this.pos.y = height;
+    }
+    if (this.pos.x > width) {
+      this.vel.x *= -1;
+      this.pos.x = width;
+    }
+    if (this.pos.x < 0){
+      this.vel.x *= -1;
+      this.pos.x = 0;
+    }
+    if (this.pos.y < 0){
+      this.vel.y *= -1;
+      this.pos.y = 0;
+    }
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,36 +1,37 @@
 let m;
 let m2;
-let movers = [];
+let movers =[];
 
 function setup() {
   createCanvas(400, 400);
+  canvas = createGraphics(400, 400);
   m = new Mover(50, 50, 10);
-  m2 = new Mover(300, 300, 60);
-  
-  for (let i = 0; i < 5; i++) {
-    let mover = new Mover(random(400), random(400), random(5, 50));
+  m2 = new Mover(300, 300, 50);
+
+  for (let i = 0; i < 5; i++){
+    let mover = new Mover(random(width), random(height), random(5, 10));
     movers.push(mover);
   }
 }
 
 function draw() {
-  background(220);
-  // for (let i = 0; i < movers.length; i++) {
-  //   let mover = movers[i];
-  //   mover.update();
-  //   mover.show();
-  // }
-  
-  for (let mover of movers) {
+  background(250);
+  image(canvas,0, 0);
+
+  for (let mover of movers){
     mover.update();
     mover.show();
+    mover.drawOn(canvas);
+    mover.edges();
   }
-  
-  if (mouseIsPressed) {
 
-    let mouse = createVector(mouseX, mouseY);
-    for (let mover of movers) {
-      mover.attractTo(mouse);
+  if (mouseIsPressed) {
+    let mousePos = createVector(mouseX, mouseY);
+    m.attractTo(mousePos);
+    m2.attractTo(mousePos);
+
+    for(let mover of movers){
+      mover.attractTo(mousePos);
     }
   }
 }
