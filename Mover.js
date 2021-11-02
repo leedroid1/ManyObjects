56bd3bacda4d5dc1a862090ae52e84a6fd5a64b9
leedroid1 <leedroid800@gmail.com>
2021-11-02 15:15:42 +0900

    Edit Mover.js, sletch.js

--- Mover.js
+++ Mover.js
@@ -1,25 +1,31 @@
 class Mover {
-    constructor(x, y, m) {
-      this.pos = createVector(x, y);
-      this.vel = createVector();
-      this.acc = createVector();
-      this.m = m;
-      
-    }
+  constructor(x, y, m) {
+    this.pos = createVector(x, y);
+    this.vel = createVector();
+    this.acc = createVector();
+    this.m = m;
     
-    applyForce(aForce) {
-      let f = p5.Vector.div(aForce, this.m);
-      this.acc.add(f);
-    }
-    
-    update() {
-      this.vel.add(this.acc);
-      this.pos.add(this.vel);
-      this.acc.set(0, 0);
-    }
-    
-    show() {
-      fill(0);
-      circle(this.pos.x, this.pos.y, this.m);
-    }
   }
+  
+  applyForce(aForce) {
+    let f = p5.Vector.div(aForce, this.m);
+    this.acc.add(f);
+  }
+  
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+  }
+  
+ attractTo(aPos) {
+    let f = p5.Vector.sub(aPos, this.pos)
+    f.limit(2);
+    this.applyForce(f);
+ }
+  
+  show() {
+    fill(0);
+    circle(this.pos.x, this.pos.y, this.m);
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,16 +1,23 @@
 let m;
+let m2;
 
 function setup() {
   createCanvas(400, 400);
   m = new Mover(50, 50, 10);
+  m2 = new Mover(300, 300, 60);
 }
 
 function draw() {
   background(220);
   m.update();
   m.show();
+  m2.update();
+  m2.show();
   
   if (mouseIsPressed) {
-    //make attract to mouse position
+
+    let mouse = createVector(mouseX, mouseY);
+    m.attractTo(mouse);
+    m2.attractTo(mouse);
   }
 }
